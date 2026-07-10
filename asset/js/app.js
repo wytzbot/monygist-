f1af13f4952dffc012c7b0f11b33875bba576034
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-10 22:11:14 +0100

    Update app.js

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,1 +1,14 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id') || 'coffee-shop';
 
+fetch('/asset/data/businesses.json')
+.then(res => res.json())
+.then(data => {
+  const biz = data.find(b => b.id === id);
+  if(!biz) return;
+  document.getElementById('pageTitle').innerText = biz.name + " | MonyGist";
+  document.getElementById('businessName').innerText = biz.name;
+  document.getElementById('businessSummary').innerText = biz.summary;
+  document.getElementById('startupCost').innerText = formatMoney(biz.startup);
+  document.getElementById('monthlyProfit').innerText = formatMoney(biz.profit);
+});
